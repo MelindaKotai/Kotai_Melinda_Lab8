47a15a9e929a765fe2b399814d60a413eb8b955b
Kotai Melinda <kotai_melinda2000@yahoo.com>
2020-11-25 17:27:55 +0200

    Add project files.

---
+++ Data/Kotai_Melinda_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Kotai_Melinda_Lab8.Models;
+
+namespace Kotai_Melinda_Lab8.Data
+{
+    public class Kotai_Melinda_Lab8Context : DbContext
+    {
+        public Kotai_Melinda_Lab8Context (DbContextOptions<Kotai_Melinda_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Kotai_Melinda_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Kotai_Melinda_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Kotai_Melinda_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Migrations/20201123160034_PublishingDate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kotai_Melinda_Lab8.Migrations
+{
+    public partial class PublishingDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PublishingDate",
+                table: "Book",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublishingDate",
+                table: "Book");
+        }
+    }
+}
